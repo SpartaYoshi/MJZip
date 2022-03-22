8190c350241ec073fb0a1916df90f6cddb2defad
itsNko <99725690+itsNko@users.noreply.github.com>
2022-03-22 18:11:41 +0100

    Deleted unrequired files.

--- A06.3/compile_filetype.sh
+++
@@ -1,3 +0,0 @@
-gcc test_filetype.c -c
-gcc mypackzip_functions.c -c
-gcc test_filetype.o mypackzip_functions.o -o test_filetype
--- A06.3/createmypackzip.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(int argc, char *argv[])
-{
-	printf("%d\n", crea_fichero(argv[1]));
-}
